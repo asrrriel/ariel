196123a6f751ab162d849725247167d11d7afe90
lolguy91 <retek1544@gmail.com>
2025-06-20 10:04:09 +0200

    Refactor and multimonitor support

--- drmtest/test.c
+++ drmtest/test.c
@@ -10,13 +10,23 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
-int main() {
 
-    int fd = open("/dev/dri/card0", O_RDWR | O_CLOEXEC);
+int init_drm() {
+    int fd = -1;
+    for (int i = 0; i < 10; i++) {
+        char name[32];
+        sprintf(name, "/dev/dri/card%d", i);
+        fd = open(name, O_RDWR | O_CLOEXEC);
+        if (fd != -1) {
+            printf("Found and opened drm device \"%s\"\n", name);
+            break;
+        }
+    }
     if (fd == -1) {
         printf("Failed to open drm device\n");
         return 1;
     }
+
     drmVersionPtr version = drmGetVersion(fd);
     if (version) {
         printf("Driver version: %d.%d.%d\n",version->version_major, version->version_minor, version->version_patchlevel);
@@ -30,34 +40,67 @@ int main() {
     int ret = drmGetCap(fd, DRM_CAP_DUMB_BUFFER, &cap);
     if (ret != 0 || cap == 0) {
         printf("Dumb buffer unsupported\n");
-        return 1;
+        return -1;
     }
-    printf("Dumb buffer supported\n");
 
-    drmModeConnector *connector = NULL;
+    return fd;
+}
 
+typedef struct {
+    int count;
+    drmModeConnector **connectors;
+} connected_connectors;
+
+connected_connectors enumerate_resources(int fd) {
     drmModeRes *resources = drmModeGetResources(fd);
+
+    if (resources->count_connectors == 0) {
+        return (connected_connectors){0, NULL};
+    }
+
+    int count_connectors = 0;
+    drmModeConnector **connectors = NULL;
+
     for (int i = 0; i < resources->count_connectors; ++i) {
         drmModeConnector *conn = drmModeGetConnector(fd, resources->connectors[i]);
         if (conn->connection == DRM_MODE_CONNECTED && conn->count_modes > 0) {
-            printf("Connector %d: %dx%d\n", i, conn->modes[0].hdisplay, conn->modes[0].vdisplay);
-            connector = conn;
-            break;
+            count_connectors++;
+            connectors = realloc(connectors, count_connectors * sizeof(drmModeConnector*));
+            connectors[count_connectors - 1] = conn;
+        } else {
+            drmModeFreeConnector(conn);
         }
     }
 
-    if (connector == NULL) {
-        printf("No connector found\n");
-        return 1;
+    for(int i = 0; i < count_connectors; i++) {
+        drmModeConnector *conn = connectors[i];
+        printf("Connector %d modes:\n", i);
+        for (int j = 0; j < conn->count_modes; ++j) {
+            printf("    Mode %d: %dx%d %dHz\n", j, conn->modes[j].hdisplay, conn->modes[j].vdisplay, conn->modes[j].vrefresh);
+        }
     }
 
-    printf("Using connector %d: %dx%d\n", connector->connector_id, connector->modes[0].hdisplay, connector->modes[0].vdisplay);
+    printf("Number of connected connectors: %d\n", count_connectors);
 
+    return (connected_connectors){count_connectors, connectors}; // WIP
+}
+
+typedef struct {
+    drmModeCrtc *crtc;
+    uint32_t handle;
+    uint32_t pitch;
+    uint64_t size;
+    uint32_t fb_id;
+    uint64_t offset;
+    uint8_t *fb_ptr;
+} framebuffer;
+
+framebuffer setup_framebuffer(int fd, drmModeConnector *connector) {
     drmModeEncoder* encoder = drmModeGetEncoder(fd, connector->encoders[0]);
 
     if (encoder == NULL) {
         printf("Failed to get encoder\n");
-        return 1;
+        return (framebuffer){};
     }
 
     printf("Using encoder %d\n", encoder->encoder_id);
@@ -66,7 +109,7 @@ int main() {
 
     if (crtc == NULL) {
         printf("Failed to get crtc\n");
-        return 1;
+        return (framebuffer){};
     }
 
     printf("Using CRTC %d\n", crtc->crtc_id);
@@ -76,36 +119,63 @@ int main() {
     uint64_t size;
     if(drmModeCreateDumbBuffer(fd, connector->modes[0].hdisplay, connector->modes[0].vdisplay, 32,0 , &handle, &pitch, &size)){
         printf("Failed to create dumb buffer\n");
-        return 1;
+        return (framebuffer){};
     }
 
     uint32_t fb;
     if (drmModeAddFB(fd, connector->modes[0].hdisplay, connector->modes[0].vdisplay, 24, 32, pitch, handle, &fb)){
         printf("Failed to add framebuffer\n");
-        return 1;
+        return (framebuffer){};
     }
 
     uint64_t offset;
     if (drmModeMapDumbBuffer(fd, handle, &offset)) {
         printf("Failed to prepare dumb buffer for use\n");
-        return 1;
+        return (framebuffer){};
     }
 
     uint8_t *fb_ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset);
     if (fb_ptr == MAP_FAILED) {
-        perror("mmap");
-        exit(1);
+        printf("Failed to mmap framebuffer\n");
+        return (framebuffer){};
     }
 
-    // fill it with white
-    memset(fb_ptr, 0xFF, size);
+    return (framebuffer){crtc,handle, pitch, size, fb, offset, fb_ptr};
+}
 
-    if(drmModeSetCrtc(fd, crtc->crtc_id, fb, 0, 0, &connector->connector_id, 1, &connector->modes[0])){
-        printf("Failed to modeset crtc\n");
+
+int main() {
+
+    int fd = init_drm();
+
+    connected_connectors connectors = enumerate_resources(fd);
+
+    if (connectors.count == 0) {
+        printf("No connector found\n");
         return 1;
     }
 
-    pause();
+    
+    if (!drmIsMaster(fd)){
+        printf("Process is not the DRM master\n");
+        return 1;
+    }
+    
+    for (int i = 0; i < connectors.count; i++) {
+        drmModeConnector *connector = connectors.connectors[i];
+        printf("Modesetting connector %d: %dx%d\n", connector->connector_id, connector->modes[0].hdisplay, connector->modes[0].vdisplay);
+
+        framebuffer fb = setup_framebuffer(fd, connector);
+
+        memset(fb.fb_ptr, 0xFF, fb.size);
+
+        if(drmModeSetCrtc(fd, fb.crtc->crtc_id, fb.fb_id, 0, 0, &connector->connector_id, 1, &connector->modes[0])){
+            printf("Failed to modeset crtc\n");
+            return 1;
+        }
+    }
+    
+    //pause();
 
     return 0;
 
