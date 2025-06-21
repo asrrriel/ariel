f36876d5003cbf91bbd565569f6a27d01d69910a
lolguy91 <retek1544@gmail.com>
2025-06-21 17:25:33 +0200

    now its rid of logic bugs and is 69 lines long

--- libutf8/utf8.h
+++ libutf8/utf8.h
@@ -14,51 +14,49 @@ typedef struct {
     uint32_t codepoint;
 } utf8_dec_state_t;
 
+/*
+ * @brief Decodes a utf8 character
+ * 
+ * @param state The state of the decoder
+ * @param c The byte to decode
+ * @param out The decoded character
+ * 
+ * @return The status code
+*/
+uint8_t utf8_decode(utf8_dec_state_t *state, uint8_t c, uint32_t *out);
+
 #ifdef __UTF8_IMPL__
-    #define __UTF8_INTERNAL_BYTEPAT_CONT 0b10000000 
-    #define __UTF8_INTERNAL_BYTEPAT_2    0b11000000
-    #define __UTF8_INTERNAL_BYTEPAT_3    0b11100000
-    #define __UTF8_INTERNAL_BYTEPAT_4    0b11110000
-    /*
-     * @brief Decodes a utf8 character
-     * 
-     * @param state The state of the decoder
-     * @param c The byte to decode
-     * @param out The decoded character
-     * 
-     * @return The status code
-    */
     uint8_t utf8_decode(utf8_dec_state_t *state, uint8_t c, uint32_t *out){
         if (state->bytes_remaining == 0){
-            if (c & __UTF8_INTERNAL_BYTEPAT_CONT){
-                if (c >= __UTF8_INTERNAL_BYTEPAT_2){
-                    state->num_bytes = 2;
-                    state->bytes_remaining = 1;
-                    state->codepoint = c & ~__UTF8_INTERNAL_BYTEPAT_2;
-                    return UTF8_MORE_BYTES_REQUIRED;
-                } else if (c >= __UTF8_INTERNAL_BYTEPAT_3){
-                    state->num_bytes = 3;
-                    state->bytes_remaining = 2;
-                    state->codepoint = c & ~__UTF8_INTERNAL_BYTEPAT_3;
-                    return UTF8_MORE_BYTES_REQUIRED;
-                } else if (c >= __UTF8_INTERNAL_BYTEPAT_4){
-                    state->num_bytes = 4;
-                    state->bytes_remaining = 3;
-                    state->codepoint = c & ~__UTF8_INTERNAL_BYTEPAT_4;
-                    return UTF8_MORE_BYTES_REQUIRED;
-                }
-            } else {
+            if (c == 0xFF) {
+                return UTF8_INVALID_INPUT;
+            } else if (c >= 0xf0){ // 4 bytes long
+                state->num_bytes = 4;
+                state->bytes_remaining = 3;
+                state->codepoint = c & 0x07;
+                return UTF8_MORE_BYTES_REQUIRED;
+            } else if (c >= 0xe0){ // 3 bytes long
+                state->num_bytes = 3;
+                state->bytes_remaining = 2;
+                state->codepoint = c & 0x0F;
+                return UTF8_MORE_BYTES_REQUIRED;
+            } else if (c >= 0xc0){ // 2 bytes long
+                state->num_bytes = 2;
+                state->bytes_remaining = 1;
+                state->codepoint = c & 0x1F;
+                return UTF8_MORE_BYTES_REQUIRED;
+            } else if (c >= 0x80){ // continuation byte, not supposed to be here
+                return UTF8_INVALID_INPUT;
+            } else { // 1 byte long(may be a non-ascii invalid character)
                 *out = (uint32_t)c;
                 return UTF8_CODEPOINT_EXTRACTED;
             }
         } else {
-            if(!(c & __UTF8_INTERNAL_BYTEPAT_CONT)){
+            if(!(c & 0x80)){
                 return UTF8_INVALID_INPUT;
             } 
-    
-            state->codepoint = (c & ~__UTF8_INTERNAL_BYTEPAT_2) | (state->codepoint << 6);
+            state->codepoint = (c & 0x3F) | (state->codepoint << 6);
             state->bytes_remaining--;
-
             if(state->bytes_remaining == 0){
                 *out = state->codepoint;
                 return UTF8_CODEPOINT_EXTRACTED;
@@ -66,17 +64,6 @@ typedef struct {
             return UTF8_MORE_BYTES_REQUIRED;
         }
     }
-#else
-    /*
-     * @brief Decodes a utf8 character
-     * 
-     * @param state The state of the decoder
-     * @param c The byte to decode
-     * @param out The decoded character
-     * 
-     * @return The status code
-    */
-    uint8_t utf8_decode(utf8_dec_state_t *state, uint8_t c, uint32_t *out);
 #endif
 
 #endif // __UTF8_H__
